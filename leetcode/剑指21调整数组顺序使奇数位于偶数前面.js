451cc9362a25373607434f7606f4d5f0b9ab3d5e
CoderDi <1138832931@qq.com>
2021-09-24 23:06:00 +0800

    update: leetcode:剑指21

--- leetcode/剑指21调整数组顺序使奇数位于偶数前面.js
+++ leetcode/剑指21调整数组顺序使奇数位于偶数前面.js
@@ -3,9 +3,19 @@
  * @return {number[]}
  */
 var exchange = function (nums) {
-  odd = nums.filter((item) => item % 2 === 1);
-  even = nums.filter((item) => item % 2 === 0);
-  return odd.concat(even);
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    while (left < right && nums[left] % 2 == 1) {
+      left++;
+    }
+    while (left < right && nums[right] % 2 == 0) {
+      right--;
+    }
+    [nums[left], nums[right]] = [nums[right], nums[left]];
+  }
+  return nums;
 };
 
 let nums = [3, 2, 1, 5, 4];
