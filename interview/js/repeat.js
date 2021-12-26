73eb1bdd227fd90a2a154aaa81c846ceda6b9f4c
dike1999 <1138832931@qq.com>
2021-12-26 13:21:55 +0800

    feat: add kusishou md

--- interview/js/repeat.js
+++ interview/js/repeat.js
@@ -2,7 +2,7 @@ function repeat(func, times, wait) {
   return function (context) {
     for (let i = 0; i < times; i++) {
       setTimeout(() => {
-        func(context);
+        func(context, i);
       }, i * wait);
     }
   };
@@ -10,4 +10,4 @@ function repeat(func, times, wait) {
 
 // 使下面调用代码能正常工作
 const repeatFunc = repeat(console.log, 4, 3000);
-repeatFunc("hello world"); //会输出4次 hello world, 每次间隔3秒
+repeatFunc('hello world'); //会输出4次 hello world, 每次间隔3秒
