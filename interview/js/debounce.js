7692f0e74b22bc12f17014ee8f964fa2b30a3d5e
dike1999 <1138832931@qq.com>
2022-01-10 15:32:12 +0800

    refactor: debounce

--- interview/js/debounce.js
+++ interview/js/debounce.js
@@ -1,13 +1,29 @@
-function debounce(func, wait) {
+/**
+ * @description 防抖函数
+ * @param {function} func 函数
+ * @param {number} wait 延迟执行毫秒数
+ * @param {boolean} immediate 是否立即执行
+ * @returns {function}
+ */
+function debounce(func, wait, immediate = false) {
   let timeout;
+
   return function () {
     let context = this;
     let args = arguments;
-    if (timeout) {
-      clearTimeout(timeout);
+
+    if (timeout) clearTimeout(timeout);
+
+    if (immediate) {
+      let callNow = !timeout;
+      timeout = setTimeout(() => {
+        timeout = null;
+      }, wait);
+      if (callNow) func.apply(context, args);
+    } else {
+      timeout = setTimeout(() => {
+        func.apply(context, args);
+      }, wait);
     }
-    timeout = setTimeout(() => {
-      func.apply(context, args);
-    }, wait);
   };
 }
